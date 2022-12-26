0cbfc295b00bc9e2662b57853780fe93ac8bdef6
DangoDaniel <dango@o2.pl>
2022-12-26 09:13:21 +0100

    commit

---
+++ src/main/java/MainDao.java
@@ -0,0 +1,64 @@
+public class MainDao {
+
+    public static void create() {
+        UserDao userDao = new UserDao();
+        User user1 = new User();
+        user1.setUserName("1arek");
+        user1.setEmail("1arkadiusz.jozwiak@coderslab.pl");
+        user1.setPassword("1pass");
+        userDao.create(user1);
+
+        User user2 = new User();
+        user2.setUserName("2arek");
+        user2.setEmail("2arkadiusz.jozwiak@coderslab.pl");
+        user2.setPassword("2pass");
+        userDao.create(user2);
+
+        User user3 = new User();
+        user3.setUserName("3arek");
+        user3.setEmail("3arkadiusz.jozwiak@coderslab.pl");
+        user3.setPassword("3pass");
+        userDao.create(user3);
+
+        User user4 = new User();
+        user4.setUserName("4arek");
+        user4.setEmail("4arkadiusz.jozwiak@coderslab.pl");
+        user4.setPassword("4pass");
+        userDao.create(user4);
+    }
+
+    public static void read(int id){
+        UserDao userDao = new UserDao();
+        User userRead = new User();
+        userRead=userDao.read(id);
+        System.out.println("id: " + userRead.getId()+
+                " email: "+ userRead.getEmail()+
+                " username: "+ userRead.getUserName()+
+                " password: "+ userRead.getPassword());
+    }
+
+    public static void update(int id) {
+        UserDao userDao = new UserDao();
+        User userToUpdate = userDao.read(id);
+        userToUpdate.setUserName("Arkadiusz");
+        userToUpdate.setEmail("arek@coderslab.pl");
+        userToUpdate.setPassword("superPassword");
+        userDao.update(userToUpdate);
+    }
+
+    public static void delete (int id) {
+        UserDao userDao = new UserDao();
+        userDao.delete(id);
+    }
+
+    public static void findAll(){
+        UserDao userDao = new UserDao();
+        User[] all = userDao.findAll();
+        for (User u : all) {
+            System.out.println("id: " + u.getId()+
+                    " email: "+ u.getEmail()+
+                    " username: "+ u.getUserName()+
+                    " password: "+ u.getPassword());
+        }
+    }
+}
---
+++ src/main/java/UserDao.java
@@ -0,0 +1,149 @@
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.DbUtil;
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.*;
+import java.util.Arrays;
+
+
+public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO workshop2.users(username, email, password) VALUES (?, ?, ?)";
+
+    private static final String UPDATE_USER_QUERY =
+            "update workshop2.users set email = ?, username = ?, password = ? where id = ?;";
+
+    private static final String SELECT_USER_BY_ID_QUERY =
+            "select * from workshop2.users where id = ?;";
+
+    private static final String DELETE_USER_BY_ID_QUERY =
+            "delete from workshop2.users where id = ?;";
+
+    private static final String SELLECT_ALL_USER_QUERY =
+            "select * from workshop2.users;";
+
+
+    public User create(User user) {
+        String hashPassword = hashPassword(user.getPassword());
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement preparedStatement=
+                    conn.prepareStatement(CREATE_USER_QUERY, PreparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, hashPassword);
+            user.setPassword(hashPassword); //zwracam do obiektu zahaszowane haslo
+
+            preparedStatement.executeUpdate();
+
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public User read(int userId) {
+        User user = new User();
+
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement getUserByIDStatement =
+                    conn.prepareStatement(SELECT_USER_BY_ID_QUERY);
+            getUserByIDStatement.setInt(1, userId);
+
+            try (ResultSet result = getUserByIDStatement.executeQuery()) {
+                result.next();
+                user.setId(result.getInt("id"));
+                user.setEmail(result.getString("email"));
+                user.setUserName(result.getString("username"));
+                user.setPassword(result.getString("password"));
+                return user;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public void update(User user) {
+// "update workshop2.users set email = ?, username = ?, password = ? where id = ?;"
+        String hashPassword = hashPassword(user.getPassword());
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement updateUserStatement =
+                    conn.prepareStatement(UPDATE_USER_QUERY);
+            updateUserStatement.setString(1, user.getEmail());
+            updateUserStatement.setString(2, user.getUserName());
+            updateUserStatement.setString(3, hashPassword);
+            updateUserStatement.setInt(4, user.getId());
+            updateUserStatement.executeUpdate();
+            user.setPassword(hashPassword);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void delete(int userId) {
+//        private static final String DELETE_USER_BY_ID_QUERY =
+//                "delete from workshop2.users where id = ?;";
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement deleteUserByIDStatement =
+                    conn.prepareStatement(DELETE_USER_BY_ID_QUERY);
+            deleteUserByIDStatement.setInt(1, userId);
+            deleteUserByIDStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public User[] findAll(){
+//        private static final String SELLECT_ALL_USER_QUERY =
+//                "select * from workshop2.users;";
+        try (Connection conn = DbUtil.getConnection()) {
+            PreparedStatement selectAllUsersStatement =
+                    conn.prepareStatement(SELLECT_ALL_USER_QUERY);
+            selectAllUsersStatement.executeQuery();
+
+//            User[] users = new User[0];
+            User[] users = {};
+
+
+            try(ResultSet resultSet = selectAllUsersStatement.executeQuery()) {
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getInt("id"));
+                    user.setEmail(resultSet.getString("email"));
+                    user.setUserName(resultSet.getString("username"));
+                    user.setPassword(resultSet.getString("password"));
+                    users = addToArray(user, users);
+                }
+                return users;
+            }catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private User[] addToArray(User u, User[] users) {
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1); // Tworzymy kopię tablicy powiększoną o 1.
+        tmpUsers[tmpUsers.length-1] = u; // Dodajemy obiekt na ostatniej pozycji.
+        return tmpUsers; //
+    }
+}
